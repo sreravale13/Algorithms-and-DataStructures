655580cce8ac91c456cba7d14d52dcbfc3fda0a9
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-05-28 13:01:03 -0700

    Time: 238 ms (26.96%) | Memory: 93.6 MB (10.72%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.js
+++ 295-find-median-from-data-stream/find-median-from-data-stream.js
@@ -21,8 +21,11 @@ var MedianFinder = function() {
  * @return {void}
  */
 MedianFinder.prototype.addNum = function(num) {
+    // small nos are added to max heap
     this.maxHeap.push(num);
+    // larger nos of list are added to min heap
     this.minHeap.push(this.maxHeap.pop());
+    // trying to maintain equal heaps with at max 1 difference in odd nos usecase
     if (this.minHeap.size() > this.maxHeap.size()) {
         this.maxHeap.push(this.minHeap.pop());
     }
@@ -34,8 +37,10 @@ MedianFinder.prototype.addNum = function(num) {
  */
 MedianFinder.prototype.findMedian = function() {
     if (this.maxHeap.size() > this.minHeap.size()) {
+        // it gives mid/max of min valuesof list when sorted as ascending as res for odd
         return this.maxHeap.top();
     }
+    // else mid value of min of maxheap+ max of min hep /2 
     return (this.maxHeap.top() + this.minHeap.top())/ 2;
 
     
